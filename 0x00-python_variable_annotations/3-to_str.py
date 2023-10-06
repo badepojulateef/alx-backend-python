c7cbf6ec3ea9770f42001f1afcb568c1626afa26
Adepoju Lateef <badepojulateef@gmail.com>
2023-10-06 03:03:42 +0100

    added task 3

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+A type-annotated function to_str that takes a float n as
+argument and returns the string representation of the float.
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Converts a float to its string representation.
+
+    Args:
+        n (float): The input float.
+
+    Returns:
+        str: The string representation of the input float.
+    """
+    return str(n)
