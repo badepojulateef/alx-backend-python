377e69c486c16d9dfdc8bd4a9b26cb06cfd9f9b8
Adepoju Lateef <badepojulateef@gmail.com>
2023-10-06 03:00:52 +0100

    added task 2

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+A type-annotated function floor which takes a float n
+as argument and returns the floor of the float.
+"""
+
+
+def floor(n: float) -> int:
+    """
+    Returns the floor (rounded down to the nearest integer) of a given float.
+
+    Args:
+        n (float): The input float.
+
+    Returns:
+        int: The floor of the input float, rounded down to the nearest integer.
+    """
+    return int(n)
