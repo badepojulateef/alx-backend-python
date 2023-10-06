6c45a5bd70c23d05a129ea041c505823f59b7cd0
Adepoju Lateef <badepojulateef@gmail.com>
2023-10-06 02:55:27 +0100

    added task 0

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -4,6 +4,7 @@ A type-annotated function concat that takes a string str1 and
 a string str2 as arguments and returns a concatenated string
 """
 
+
 def concat(str1: str, str2: str) -> str:
     """
     Concatenates two strings together.
