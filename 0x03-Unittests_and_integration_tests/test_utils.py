6cfe9c11e849e74c7df0031c6ffb8117607c247d
Adepoju Lateef <badepojulateef@gmail.com>
2023-10-30 20:42:00 +0100

    Added README.md

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+"""Familiarize yourself with the utils.access_nested_map function and
+understand its purpose. Play with it in the Python console to make
+sure you understand.
+
+In this task you will write the first unit test for utils.access_nested_map.
+
+Create a TestAccessNestedMap class that inherits from unittest.TestCase.
+
+Implement the TestAccessNestedMap.test_access_nested_map method to test that
+the method returns what it is supposed to.
+
+Decorate the method with @parameterized.expand to test the function for
+ollowing inputs:
+
+nested_map={"a": 1}, path=("a",)
+nested_map={"a": {"b": 2}}, path=("a",)
+nested_map={"a": {"b": 2}}, path=("a", "b")
+"""
+
+import unittest
+from unittest.mock import patch, MOCK
+import utils
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """Test class for the access_nested_map function."""
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
+    ])
+
+    def test_access_nested_map(self, nested_map, path, expected_result):
+        """Test the access_nested_map function."""
+        self.assertEqual(utils.access_nested_map(nested_map, path),
+                         expected_result)
+
+        @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b"))
+    ])
+
+    def test_access_nested_map_exception(self, nested_map, path):
+        """Test exception handling in access_nested_map function."""
+        with self.assertRaises(KeyError):
+            utils.access_nested_map(nested_map, path)
